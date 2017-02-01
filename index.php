56d75f0224a2b0738e510b42cb5db84f05acc0a2
Trevor Jex <jextrevor@gmail.com>
2017-02-01 16:10:13 -0700

    Moved code

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+$counter_name = "counter.txt";
+
+// Check if a text file exists. If not create one and initialize it to zero.
+if (!file_exists($counter_name)) {
+  $f = fopen($counter_name, "w");
+  fwrite($f,"0");
+  fclose($f);
+}
+
+// Read the current value of our counter file
+$f = fopen($counter_name,"r");
+$counterVal = fread($f, filesize($counter_name));
+fclose($f);
+
+// Has visitor been counted in this session?
+// If not, increase counter value by one
+if(!isset($_SESSION['hasVisited'])){
+  $_SESSION['hasVisited']="yes";
+  $counterVal++;
+  $f = fopen($counter_name, "w");
+  fwrite($f, $counterVal);
+  fclose($f); 
+}
+?>
+
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
@@ -147,33 +174,7 @@ img{
   <center><h1 id="hi"></h1></center>
   <h1 class="awesome">You are awesome!</h1>
   <div class="footer">
-  <?php
-session_start();
-$counter_name = "counter.txt";
-
-// Check if a text file exists. If not create one and initialize it to zero.
-if (!file_exists($counter_name)) {
-  $f = fopen($counter_name, "w");
-  fwrite($f,"0");
-  fclose($f);
-}
-
-// Read the current value of our counter file
-$f = fopen($counter_name,"r");
-$counterVal = fread($f, filesize($counter_name));
-fclose($f);
-
-// Has visitor been counted in this session?
-// If not, increase counter value by one
-if(!isset($_SESSION['hasVisited'])){
-  $_SESSION['hasVisited']="yes";
-  $counterVal++;
-  $f = fopen($counter_name, "w");
-  fwrite($f, $counterVal);
-  fclose($f); 
-}
-
-echo "You are visitor number $counterVal to this site";?>
+  <?php echo "You are visitor number $counterVal to this site";?>
 </div>
 </body>
 </html>
